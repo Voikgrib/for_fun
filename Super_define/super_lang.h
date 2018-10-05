e434a194d61614292490e2cf875657455e1f8246
Vladimir <32133129+Voikgrib@users.noreply.github.com>
2018-10-05 13:19:19 +0300

    Add files via upload

---
+++ Super_define/super_lang.h
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+#define здрасте 		int main()
+#define типа 			{
+#define блять 			}
+#define начинай 		(
+#define смотри	 		(
+#define посмотрел		)
+#define кончай 			)
+#define ёпт 			;
+
+#define базарь 			printf
+#define громко(arg) 	#arg "\n"
+#define слухаю			getchar()
+
+#define если 			if
+#define какого			else
+
+#define факт 			bool
+#define звук			char
+#define думай(arg)		arg
+
+#define чётко 			true
+#define зашквар 		false
+
+#define есть 			==
+#define нихуя 			!=
+#define не 				!
+#define это 			=
